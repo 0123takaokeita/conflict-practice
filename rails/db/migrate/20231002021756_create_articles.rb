edd62641cce290d25fa2413c80cea7b5cec8c9e6
KeitaTakao <k.takao@neogenia.co.jp>
2023-10-02 11:20:34 +0900

    feat: add article migration

--- rails/db/migrate/20231002021756_create_articles.rb
+++ rails/db/migrate/20231002021756_create_articles.rb
@@ -1,6 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
+      t.title :string, null: false
+      t.text :body, null: false
 
       t.timestamps
     end
